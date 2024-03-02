ef12523e80fb022545f5f6fe4a1e402375182b77
Harsh Userethe <useretheharsh2211@gmail.com>
2024-03-02 15:43:57 +0530

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,17 @@ import List from './components/List'
 import CIcon from '@coreui/icons-react';
 import * as icon from '@coreui/icons';
 import Admin from './components/Admin';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import UpdateForm from './components/UpdateForm';
 import NewFlightForm from './components/NewFlightForm';
 
 function App() {
 
+  const navigate = useNavigate();
 
+  const navigateTo = () =>{
+    navigate("/admin/login");
+  }
   return (
     <Router>
     
@@ -19,6 +22,7 @@ function App() {
       <div className="top-heading-bar"> 
         <div className="icon"><CIcon icon={icon.cilFlightTakeoff} style={{color:"#fff", fontSize: "2vw !important"}} /></div>
         <h2>Jay Prakash Narayan International Airport</h2>
+        <span onClick={navigateTo}>Admin</span>
       </div>
       <FlightsTable />
       
